3c5a79e2d0885fbee36bf1c1525f8627c9d0fca9
David Hunt <bitbucket.davidhunt@holodyn.com>
2018-10-17 14:41:01 -0400

    Updated Processor

--- daux/Processor.php
+++ daux/Processor.php
@@ -1,11 +1,16 @@
 <?php namespace Todaymade\Daux\Extension;
 
 use Todaymade\Daux\Tree\Root;
+use WebuddhaInc\CommonMark\CodeExtension;
 
 class Processor extends \Todaymade\Daux\Processor {
 
-    public function manipulateTree(Root $root)
+    public function extendCommonMarkEnvironment($environment)
     {
-        print_r($root->dump());
+
+      $ext = new \WebuddhaInc\CommonMark\CodeExtension\CodeExtension();
+      $environment->addExtension( $ext );
+
     }
+
 }
